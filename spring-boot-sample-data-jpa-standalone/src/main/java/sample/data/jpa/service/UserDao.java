6036ad841218fb59cfbbb3ef98874e6eb4ef6d13
Olivier Barais <barais@irisa.fr>
2016-09-14 14:03:49 +0200

    update sample

---
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/service/UserDao.java
@@ -0,0 +1,20 @@
+package sample.data.jpa.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import sample.data.jpa.domain.User;
+
+// Imports ...
+
+@Transactional
+public interface UserDao extends JpaRepository<User, Long> {
+
+  /**
+   * This method will find an User instance in the database by its email.
+   * Note that this method is not implemented and its working code will be
+   * automagically generated from its signature by Spring Data JPA.
+   */
+  public User findByEmail(String email);
+
+}
